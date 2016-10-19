a6173eff82507ab6943881960f27cb5ae3a24cf9
juramirezp <jua.ramirezp@gmail.com>
2016-10-19 12:10:08 -0300

    Actualización del proyecto

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".line").fadeOut();
+    $("#preloader").delay(500).fadeOut("slow");
+});
+
+
+$(window).load(function(){
+    $(".line").fadeOut();
+    $("#preloader").delay(500).fadeOut("slow");
+});
