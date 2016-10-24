8630848a7b169a4962a3167f80c306c136a82706
juramirezp <jua.ramirezp@gmail.com>
2016-10-24 14:57:11 -0300

    Comienzo de implementacion scroll

--- js/funciones.js
+++ js/funciones.js
@@ -8,3 +8,18 @@ $(window).load(function(){
     $(".line").fadeOut();
     $("#preloader").delay(500).fadeOut("slow");
 });
+
+$(document).on("scroll", function(){
+    //sacamos el desplazamiento actual de la página
+    var desplazamientoActual = $(document).scrollTop();
+    //accedemos al control de "ir arriba"
+    var topFooter = $(footer).offset().top;
+    //compruebo si debo mostrar el botón
+    if(desplazamientoActual > topFooter){
+        $(body).css("background-color","red");
+    }
+    //controlo si debo ocultar el botón
+    if(desplazamientoActual < topFooter){
+        $(body).css("background-color","green");
+    }
+});
